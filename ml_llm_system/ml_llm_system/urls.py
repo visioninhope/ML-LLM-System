e16af6c3ff4a8a1e4d8ed345e78cc83338ac01ef
Daniel Ernesto Thompson <97011815+DanielErnestoThompson@users.noreply.github.com>
2023-06-26 20:25:06 -0600

    fixed

---
+++ ml_llm_system/ml_llm_system/urls.py
@@ -0,0 +1,9 @@
+# ml_llm_system/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+]
